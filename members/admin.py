0ef1d0e41de59554ac0e879c0249639b335308f7
Nik <nikolaitomov25@gmail.com>
2025-02-07 00:33:42 +0200

    added 404 page and test page, added superuser and Member db to admin display

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models  import Member
 
 # Register your models here.
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("id", "firstName", "lastName", "phone", "joinedDate")
+
+admin.site.register(Member, MemberAdmin)
